33720edd982f200554c74ef1216086681a4e2d00
UncleGrandpa925 <vuonglongtoan@gmail.com>
2025-02-20 10:02:53 +0800

    Add comments & examples

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,15 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     hardhat: {
       forking: {
-        url: 'https://rpc.ankr.com/eth',
+        url: process.env.ETH_RPC!,
       },
       chainId: 1,
       accounts: [{
@@ -17,8 +20,8 @@ const config: HardhatUserConfig = {
 
     // FILL IN YOUR NETWORK & ACCOUNT INFORMATION HERE
     mainnet: {
-      url: 'https://rpc.ankr.com/eth',
-      accounts: ['0x'],
+      url: process.env.ETH_RPC!,
+      accounts: [`${process.env.PRIVATE_KEYS}`],
     }
   }
 };
